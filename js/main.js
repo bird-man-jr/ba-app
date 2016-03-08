1b499add6068c5adf06b4505ec4cc7fb78b76258
Greg's macbook pro <gas222@cornell.edu>
2016-03-07 20:42:21 -0500

    Logic for drag and drop. This api sucks.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 var App = {
 
+  points: 0,
+  seconds: 120,
+
   init: function() {
     new App.RecipesView();
   }
@@ -13,7 +16,6 @@ App.RecipeCollection = Backbone.Collection.extend({
   model: App.Recipe,
   url: "https://ba-js-test.herokuapp.com/api/menu_next_week",
   parse: function (response) {
-    console.log('parsing ...');
     return response.two_person_plan.recipes;
   }
 });
@@ -34,8 +36,6 @@ App.RecipesView = Backbone.View.extend({
 
   render:function (model) {
       _.each(model, function (recipe) {
-        console.log("passed");
-        console.log(recipe);
         new App.RecipeView({model:recipe}).render();
         new App.CardView({model:recipe}).render();
       }, this);
@@ -46,12 +46,47 @@ App.RecipesView = Backbone.View.extend({
 
 App.RecipeView = Backbone.View.extend({
 
+  el: '#recipes',
+
   tagName: 'div',
 
   template:_.template($('#tpl-recipe-item').html()),
 
+  events: {
+   'dragenter .recipe' : 'dragEnterHandler',
+   'dragover .recipe' : 'dragOverHandler',
+   'dragleave .recipe' : 'dragLeaveHandler',
+   'drop .recipe' : 'dropHandler'
+  },
+
+  dragEnterHandler: function(e) {
+    e.preventDefault();
+    $(e.target).addClass('selected');
+    return false;
+  },
+
+  dragOverHandler: function(e) {
+    e.preventDefault();
+    return false;
+  },
+
+  dragLeaveHandler: function(e) {
+    $(e.target).removeClass('selected');
+  },
+
+  dropHandler: function(e) {
+    e.preventDefault();
+    var targetId = $(e.target).parent().attr('data-product-id');
+    var itemDragged = e.originalEvent.dataTransfer.getData("text/plain");
+    if (targetId === itemDragged) {
+
+    };
+    $(e.target).removeClass('selected');
+    return false;
+  },
+
   render:function () {
-    $('#recipes').append(this.template(this.model.attributes.recipe));
+    $(this.$el).append(this.template(this.model.attributes.recipe));
     return this;
   }
 
@@ -59,13 +94,24 @@ App.RecipeView = Backbone.View.extend({
 
 App.CardView = Backbone.View.extend({
 
+  el: '#cards',
+
   tagName: 'div',
 
-  template:_.template($('#tpl-card-item').html()),
+  template: _.template($('#tpl-card-item').html()),
+
+  events: {
+   'dragstart .card' : 'dragStartHandler'
+  },
+
+  dragStartHandler: function(e) {
+    var itemDragged = $(e.target).attr('data-product-id');
+    event.dataTransfer.setData('text/plain', itemDragged);
+  },
 
   render:function () {
-    $('#cards').append(this.template(this.model.attributes.recipe));
-    return this;
+    $(this.$el).append(this.template(this.model.attributes.recipe));
+    return;
   }
 
 });
